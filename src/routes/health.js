3a08f6e0eb63337156233291cbd10cfa947ecd9f
rishikrohan <rishik.rohan09@gmail.com>
2021-11-03 12:22:37 +0530

    fix: remove validator from health route
    Signed-off-by: rishikrohan <rishik.rohan09@gmail.com>

--- src/routes/health.js
+++ src/routes/health.js
@@ -1,19 +1,13 @@
 const Joi = require('joi');
 const { health } = require('../controllers/health');
 
-const validate =  {
-  params: Joi.object({
-    value: Joi.number().integer().min(1).max(100).default(10)
-  })
-}
 module.exports = [
   {
     method: 'GET',
     path: '/health',
     options: {
       auth: false,
-      tags: ['api', 'health'],
-      validate
+      tags: ['api', 'health']
     },
     handler: health
   }
